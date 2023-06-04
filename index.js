e2bbf146925d7f3e2bcf9e6b836129854a2906bd
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-04 14:55:08 +0400

    updated index

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ import { dbConnect } from './config/dbConnect.js'
 import categoryRouter from './routes/category.js'
 import productRouter from './routes/product.js'
 import userRouter from './routes/user.js'
+import basketRouter from './routes/basket.js'
 
+// Configuration
 const app = express()
 dbConnect()
-
 app.use(express.json())
 
 // Routers
 app.use('/api/category/', categoryRouter)
 app.use('/api/product/', productRouter)
-app.use('/api/basket/', userRouter)
+app.use('/api/basket/', basketRouter)
+app.use('/api/user/', userRouter)
 
 // Server
 app.listen(process.env.PORT, () => {
