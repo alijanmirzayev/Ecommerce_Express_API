2740f9b8b0db0bb5ec1be2091a5b1cabb315ffd5
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-04 14:36:29 +0400

    updated

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import env from 'dotenv/config'
 import { dbConnect } from './config/dbConnect.js'
+import categoryRouter from './routes/category.js'
+import productRouter from './routes/product.js'
+import userRouter from './routes/user.js'
 
 const app = express()
 dbConnect()
 
 app.use(express.json())
 
+// Routers
+app.use('/api/category/', categoryRouter)
+app.use('/api/product/', productRouter)
+app.use('/api/basket/', userRouter)
+
+// Server
 app.listen(process.env.PORT, () => {
     console.log('Server is running!')
 })
